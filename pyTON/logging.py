91f53a27b5abeb3c7e8bab7f88fd2c5e08b61f5a
dungeon-master-666 <dungeon666master@protonmail.com>
2022-02-04 18:32:32 +0300

    RateLimit in middleware

--- pyTON/logging.py
+++ pyTON/logging.py
@@ -1,7 +1,7 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
-from starlette.responses import Response
+from starlette.responses import Response, StreamingResponse
 from starlette.types import Message
 
 from pymongo import MongoClient
@@ -9,10 +9,16 @@ from datetime import datetime
 from functools import wraps
 from json import loads
 
+from limits import RateLimitItem, parse_many
+from limits.aio.storage import RedisStorage
+from limits.aio.strategies import FixedWindowRateLimiter
+
+from pyTON.api_key_manager import per_method_limits, total_limits, api_key_from_request, api_key_header, api_key_query
 from pyTON.models import TonResponse
 from config import settings
 
 from loguru import logger
+
 
 
 def to_mongodb(collection, creds):
@@ -41,18 +47,24 @@ def generic_exception_handler(exc):
     return JSONResponse(res.dict(exclude_none=True), status_code=status.HTTP_503_SERVICE_UNAVAILABLE)
     
 
-class MongoLoggerMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app, host, port, username, password_file, database):
+class LoggerAndRateLimitMiddleware(BaseHTTPMiddleware):
+    def __init__(self, app, mongo_host, mongo_port, mongo_username, mongo_password_file, mongo_database,
+        rate_limit_redis_uri, endpoints, rate_limit_enabled=True):
         super().__init__(app)
-        with open(password_file, 'r') as f:
+        with open(mongo_password_file, 'r') as f:
             password = f.read()
-        self.client = MongoClient(host=host, 
-                                  port=port,
-                                  username=username,
+        self.client = MongoClient(host=mongo_host, 
+                                  port=mongo_port,
+                                  username=mongo_username,
                                   password=password)
-        self.database = database
+        self.database = mongo_database
 
-    # Workaround for https://github.com/tiangolo/fastapi/issues/394
+        self.endpoints = endpoints
+        self.rate_limit_storage = RedisStorage(rate_limit_redis_uri)
+        self.fixed_window = FixedWindowRateLimiter(self.rate_limit_storage)
+        self.rate_limit_enabled = rate_limit_enabled
+
+    # Workaround for https://github.com/tiangolo/fastapi/issues/394#issuecomment-927272627
     async def set_body(self, request: Request):
         receive_ = await request._receive()
 
@@ -60,6 +72,63 @@ class MongoLoggerMiddleware(BaseHTTPMiddleware):
             return receive_
 
         request._receive = receive
+
+    async def rate_limit_and_call(self, request: Request, call_next):
+        if not self.rate_limit_enabled:
+            return await call_next(request)
+
+        path_comps = list(filter(None, request.url.path.split('/')))
+        if len(path_comps) == 0:
+            return await call_next(request)
+        endpoint = path_comps[-1]
+        if endpoint == 'jsonRPC':
+            body = await request.json()
+            endpoint = body.get('method')
+
+        if endpoint not in self.endpoints:
+            return await call_next(request)
+
+        keys = api_key_from_request(request)
+        if not isinstance(keys, list):
+            keys = [keys]
+
+        failed_limit = None
+        for key in keys:
+            # Per method limits
+            per_method_limits_str = per_method_limits(endpoint, key)
+            if per_method_limits_str == 'unlimited':
+                per_method_limits_ = []
+            else:    
+                per_method_limits_ = parse_many(per_method_limits_str)
+
+            for limit in per_method_limits_:
+                identifiers = [endpoint, key]
+                if not await self.fixed_window.hit(limit, *identifiers):
+                    failed_limit = limit
+                    break
+            if failed_limit:
+                break
+
+            # Total limits
+            total_limits_str = total_limits(key)
+            if total_limits_str == 'unlimited':
+                total_limits_ = []
+            else:    
+                total_limits_ = parse_many(total_limits_str)
+
+            for limit in total_limits_:
+                identifiers = [key]
+                if not await self.fixed_window.hit(limit, *identifiers):
+                    failed_limit = limit
+                    break
+            if failed_limit:
+                break
+
+        if failed_limit:
+            res = TonResponse(ok=False, error=f"Rate limit exceeded: {failed_limit}", code=429)
+            return JSONResponse(res.dict(exclude_none=True), status_code=res.code)
+        result = await call_next(request)
+        return result
 
     async def dispatch(self, request: Request, call_next):
         start = datetime.utcnow()
@@ -69,20 +138,21 @@ class MongoLoggerMiddleware(BaseHTTPMiddleware):
 
         response = None
         try:
-            response = await call_next(request)
+            response = await self.rate_limit_and_call(request, call_next)
         except Exception as ex:
             response = generic_exception_handler(ex)
         else:
-            response_headers = dict(response.headers.items())
-            response_body = b''
-            async for chunk in response.body_iterator:
-                response_body += chunk
-            response = Response(
-                content=response_body,
-                status_code=response.status_code,
-                headers=dict(response.headers),
-                media_type=response.media_type
-            )
+            if isinstance(response, StreamingResponse):
+                response_headers = dict(response.headers.items())
+                response_body = b''
+                async for chunk in response.body_iterator:
+                    response_body += chunk
+                response = Response(
+                    content=response_body,
+                    status_code=response.status_code,
+                    headers=dict(response.headers),
+                    media_type=response.media_type
+                )
 
         end = datetime.utcnow()
         elapsed = (end - start).total_seconds()
@@ -126,6 +196,7 @@ class MongoLoggerMiddleware(BaseHTTPMiddleware):
             'from_ip': request.headers.get('x-real-ip', '?'),
             'referer': request.headers.get('referer', '?'),
             'origin': request.headers.get('origin', '?'),
+            'api_key': request.query_params.get(api_key_query.model.name) or request.headers.get(api_key_header.model.name),
             'url': url,
             'status_code': response.status_code,
             'elapsed': elapsed
