7057bd255552a9969074607a15e8f9842f2f46de
dungeon-master-666 <dungeon666master@protonmail.com>
2022-02-23 14:52:14 +0100

    turn off allowing only cloudflare ips for now

--- infrastructure/nginx/update_cloudflare.sh
+++ infrastructure/nginx/update_cloudflare.sh
@@ -8,20 +8,22 @@ echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
 for i in `curl -sL https://www.cloudflare.com/ips-v4`; do
     echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-    echo "allow $i;" >> $CLOUDFLARE_FILE_PATH;
+    # echo "allow $i;" >> $CLOUDFLARE_FILE_PATH;
 done
 
 echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
 for i in `curl -sL https://www.cloudflare.com/ips-v6`; do
     echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-    echo "allow $i;" >> $CLOUDFLARE_FILE_PATH;
+    # echo "allow $i;" >> $CLOUDFLARE_FILE_PATH;
 done
 
 echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
-echo "" >> $CLOUDFLARE_FILE_PATH;
-echo "deny all;" >> $CLOUDFLARE_FILE_PATH;
+# echo "" >> $CLOUDFLARE_FILE_PATH;
+# echo "deny all;" >> $CLOUDFLARE_FILE_PATH;
 
-#test configuration and reload nginx
-# nginx -s reload
+# reload nginx
+if [[ $1 == "--reload" ]] ; then
+    nginx -s reload
+fi
