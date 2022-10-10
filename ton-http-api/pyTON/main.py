11b4b28b585ff622c4d35bb6938e48d74e969416
K-Dimentional Tree <kdimentionaltree@gmail.com>
2022-10-10 17:01:12 +0000

    Fixed main.py: removed import, added blank line

--- ton-http-api/pyTON/main.py
+++ ton-http-api/pyTON/main.py
@@ -1,6 +1,6 @@
-from xml.etree.ElementInclude import FatalIncludeError
 from fastapi import FastAPI
 from pyTON.api.api_v3.app import app as app_v3
+
 
 app = FastAPI()
 app.mount('/api/v3', app_v3, name='api_v3')
