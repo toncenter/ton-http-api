272b6aabe5c8f910b2dcf6a710015cb54dd028aa
K-Dimentional Tree <kdimentionaltree@gmail.com>
2022-10-17 14:19:23 +0000

    http port variable in entrypoint

--- ton-http-api/.docker/entrypoint.sh
+++ ton-http-api/.docker/entrypoint.sh
@@ -6,4 +6,4 @@ echo "Running api with ${TON_API_WEBSERVERS_WORKERS:-1} workers"
 echo "ENVIRONMENT:"
 printenv
 
-gunicorn -k uvicorn.workers.UvicornWorker -w ${TON_API_WEBSERVERS_WORKERS:-1} --bind 0.0.0.0:8081 pyTON.main:app
+gunicorn -k uvicorn.workers.UvicornWorker -w ${TON_API_WEBSERVERS_WORKERS:-1} --bind 0.0.0.0:${TON_API_HTTP_PORT:-8081} ${TON_API_GUNICORN_FLAGS} pyTON.main:app
