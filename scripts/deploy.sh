2257be1be2a8f395769777326294d1e8d734c915
k-dimentional tree <98183693+kdimentionaltree@users.noreply.github.com>
2023-05-03 14:20:46 +0200

    V3 deploy update (#85)
    * Added Dockerfile with tonlib builder
    * Update readme file
    * Updated swarm compose file

--- scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-docker stack deploy -c <(scripts/swarm_config.sh) ton-http-api
--- scripts/swarm_config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-docker-compose config | sed -E "s/cpus: ([0-9\\.]+)/cpus: '\\1'/"
