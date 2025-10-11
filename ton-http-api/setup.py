16cd6ce74341d7269d9d499e60c294473b073076
K-Dimentional Tree <kdimentionaltree@gmail.com>
2025-10-12 01:55:51 +0300

    Updated pytonlib for tvm 12 support in tonlib

--- ton-http-api/setup.py
+++ ton-http-api/setup.py
@@ -24,7 +24,7 @@ setup(
         'ring==0.10.1',
         'uvicorn==0.17.6',
         'gunicorn==20.1.0',
-        'pytonlib==0.0.64',
+        'pytonlib==0.0.67',
         'inject==4.3.1'
     ],
     package_data={},
