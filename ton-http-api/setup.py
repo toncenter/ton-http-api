87c75e2cb6f67036b41e0fc5e1db1f72040b54c4
K-Dimentional Tree <kdimentionaltree@gmail.com>
2026-02-06 15:08:47 +0300

    Updated pytonlib

--- ton-http-api/setup.py
+++ ton-http-api/setup.py
@@ -24,7 +24,7 @@ setup(
         'ring==0.10.1',
         'uvicorn==0.17.6',
         'gunicorn==20.1.0',
-        'pytonlib==0.0.69',
+        'pytonlib==0.0.70',
         'inject==4.3.1'
     ],
     package_data={},
